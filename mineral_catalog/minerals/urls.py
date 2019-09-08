f389d77b918f60d46bb5cfcbe4c0b04dd4aebad6
yi ching kang <frankyi7878@gmail.com>
2019-09-08 13:15:51 +0800

    link on home page
    add a link on home page

--- mineral_catalog/minerals/urls.py
+++ mineral_catalog/minerals/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'minerals'
 
 urlpatterns = [
-    path('',views.minerals_create),
+    path('create_database',views.minerals_create),
     path('list', views.mineral_list, name="list"),
     path('detail/<int:pk>', views.mineral_detail, name="detail"),
     path('random/detail', views.mineral_random_detail, name="random"),
